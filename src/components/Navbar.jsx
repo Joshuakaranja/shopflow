1a3346700e00363e39a1f3fb5996b916fbf77077
Joshua Karanja <jokaranja26@gmail.com>
2026-05-21 22:28:40 +0300

    creating the contents of the home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { IoIosSearch } from "react-icons/io";
 import { TiHeart } from "react-icons/ti";
 import { BsHandbag } from "react-icons/bs";
 const Navbar = () => {
+    const linkClass="py-3 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 "
   return (
     <nav className='bg-slate-800 shadow-lg flex items-center justify-around py-3 px-32 fixed top-3 left-0 w-full'>
         <Link to='/'>
@@ -16,44 +17,29 @@ const Navbar = () => {
         </span>
         </Link>
         <div className='flex items-center gap-5 text-black'>
-            <Link to='/' className='py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 '>
+            <Link to='/' className={linkClass}>
                 Home
             </Link>
-            <Link to='/about' className='py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 '>
+            <Link to='/about' className={linkClass}>
                 About
             </Link>
-            <Link to='/contact' className='py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 '>
+            <Link to='/contact' className={linkClass}>
                 Contact
             </Link>
-            <Link to='/products' className='py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 '>
+            <Link to='/products' className={linkClass}>
                 Products
             </Link>
-
         </div>
-
-        <div className='flex items-center gap-4'>
+        <div className='flex items-end gap-4'>
             <IoIosSearch className='text-white text-xl cursor-pointer hover:text-sky-300 transition duration-300'/>
             <TiHeart className='text-white text-xl cursor-pointer hover:text-sky-300 transition duration-300' />
             <div className='relative'>
                 <BsHandbag className='text-white text-xl cursor-pointer hover:text-sky-300 transition duration-300'/>
                     <span className='absolute -top-2 -right-2 bg-sky-400 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center'>0
-
                     </span>
-
-
             </div>
-
-            
-
-
-
-
-
         </div>
-
-
-    </nav>
-
+ </nav>
   )
 }
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,67 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='min-h-screen bg-slate-50 w-full flex items-center justify-center'>
-        <div className='flex flex-col items-start max-w-3xl gap-5'>
-            <h1 className='text-6xl font-bold text-black text-center mt-20 '>Home Page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsa, quam quidem soluta vel autem enim quas recusandae praesentium atque! Quidem cum debitis similique nisi ex mollitia soluta, aliquam dolorum.</p>
-    
+    <div className='min-h-screen bg-white'>
+
+      {/* Announcement bar */}
+      <div className=''>
+        <div className=''>
+          Free delivery within Nairobi &nbsp;&nbsp;•&nbsp;&nbsp; 
+          50% off sales for Kids &nbsp;&nbsp;•&nbsp;&nbsp; 
+          50% off sales for Women &nbsp;&nbsp;•&nbsp;&nbsp;
+          Free delivery within Nairobi &nbsp;&nbsp;•&nbsp;&nbsp; 
+          50% off sales for Kids &nbsp;&nbsp;•&nbsp;&nbsp; 
+          50% off sales for Women &nbsp;&nbsp;•&nbsp;&nbsp;
         </div>
+      </div>
+
+      {/* Hero Section */}
+      <section className=''>
+
+        {/* Background images side by side */}
+        <div className=''>
+          <img
+            src='https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?w=800'
+            alt='Gentleman in old money suit'
+            className=''
+          />
+          <img
+            src='https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=800'
+            alt='Lady in beach outfit'
+            className=''
+          />
+        </div>
+
+        {/* Dark overlay so text is readable */}
+        <div className='absolute inset-0 bg-black/30' />
+
+        {/* Bottom right content */}
+        <div className='relative z-10 flex flex-col items-end gap-4'>
+          <h1 className='text-white text-6xl font-light tracking-widest text-right'>
+            SHOPFLOW
+          </h1>
+          <p className='text-white/70 text-sm tracking-widest text-right'>
+            DRESS THE WAY YOU FEEL
+          </p>
+          <div className='flex items-center gap-4 mt-4'>
+            <Link
+              to='/collections'
+              className='text-white text-sm tracking-widest border-b border-white/50 hover:border-white transition duration-300'
+            >
+              DISCOVER MORE
+            </Link>
+            <Link
+              to='/products'
+              className='bg-white text-black text-sm tracking-widest px-8 py-3 hover:bg-slate-100 transition duration-300'
+            >
+              SHOP NOW
+            </Link>
+          </div>
+        </div>
+
+      </section>
+
     </div>
   )
 }
